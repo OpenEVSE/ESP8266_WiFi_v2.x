6984d057b15e8e8e3e7aafdaa83028511c981bdd
Adam Chasen <adam@chasen.name>
2025-01-23 10:05:18 -0500

    ignore static gui files copied from gui submodule and bump gui to latest on v2 branch

--- src/web_static/web_server_static_files.h
+++ src/web_static/web_server_static_files.h
@@ -7,6 +7,8 @@
 #include "web_server.lib.js.h"
 #include "web_server.ohm.jpg.h"
 #include "web_server.style.css.h"
+#include "web_server.term.html.h"
+#include "web_server.term.js.h"
 #include "web_server.wifi_portal.html.h"
 #include "web_server.wifi_portal.js.h"
 #include "web_server.wifi_signal_1.svg.h"
@@ -14,6 +16,7 @@
 #include "web_server.wifi_signal_3.svg.h"
 #include "web_server.wifi_signal_4.svg.h"
 #include "web_server.wifi_signal_5.svg.h"
+#include "web_server.zones.json.h"
 StaticFile staticFiles[] = {
   { "/assets.js", CONTENT_ASSETS_JS, sizeof(CONTENT_ASSETS_JS) - 1, _CONTENT_TYPE_JS },
   { "/emoncms.jpg", CONTENT_EMONCMS_JPG, sizeof(CONTENT_EMONCMS_JPG) - 1, _CONTENT_TYPE_JPEG },
@@ -24,6 +27,8 @@ StaticFile staticFiles[] = {
   { "/lib.js", CONTENT_LIB_JS, sizeof(CONTENT_LIB_JS) - 1, _CONTENT_TYPE_JS },
   { "/ohm.jpg", CONTENT_OHM_JPG, sizeof(CONTENT_OHM_JPG) - 1, _CONTENT_TYPE_JPEG },
   { "/style.css", CONTENT_STYLE_CSS, sizeof(CONTENT_STYLE_CSS) - 1, _CONTENT_TYPE_CSS },
+  { "/term.html", CONTENT_TERM_HTML, sizeof(CONTENT_TERM_HTML) - 1, _CONTENT_TYPE_HTML },
+  { "/term.js", CONTENT_TERM_JS, sizeof(CONTENT_TERM_JS) - 1, _CONTENT_TYPE_JS },
   { "/wifi_portal.html", CONTENT_WIFI_PORTAL_HTML, sizeof(CONTENT_WIFI_PORTAL_HTML) - 1, _CONTENT_TYPE_HTML },
   { "/wifi_portal.js", CONTENT_WIFI_PORTAL_JS, sizeof(CONTENT_WIFI_PORTAL_JS) - 1, _CONTENT_TYPE_JS },
   { "/wifi_signal_1.svg", CONTENT_WIFI_SIGNAL_1_SVG, sizeof(CONTENT_WIFI_SIGNAL_1_SVG) - 1, _CONTENT_TYPE_SVG },
@@ -31,4 +36,5 @@ StaticFile staticFiles[] = {
   { "/wifi_signal_3.svg", CONTENT_WIFI_SIGNAL_3_SVG, sizeof(CONTENT_WIFI_SIGNAL_3_SVG) - 1, _CONTENT_TYPE_SVG },
   { "/wifi_signal_4.svg", CONTENT_WIFI_SIGNAL_4_SVG, sizeof(CONTENT_WIFI_SIGNAL_4_SVG) - 1, _CONTENT_TYPE_SVG },
   { "/wifi_signal_5.svg", CONTENT_WIFI_SIGNAL_5_SVG, sizeof(CONTENT_WIFI_SIGNAL_5_SVG) - 1, _CONTENT_TYPE_SVG },
+  { "/zones.json", CONTENT_ZONES_JSON, sizeof(CONTENT_ZONES_JSON) - 1, _CONTENT_TYPE_TEXT },
 };
